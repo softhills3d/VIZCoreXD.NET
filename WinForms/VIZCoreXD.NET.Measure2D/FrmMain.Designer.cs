b65c9903e4bd80f5313a02942a92730a31990f55
YunSeokBeen <sbyoon@softhills.onmicrosoft.com>
2024-07-15 16:37:28 +0900

    VIZCoreXD.NET Measure2D

---
+++ WinForms/VIZCoreXD.NET.Measure2D/FrmMain.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace VIZCoreXD.NET.Sample
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCross = new System.Windows.Forms.Button();
+            this.btnBracket = new System.Windows.Forms.Button();
+            this.btnCurve = new System.Windows.Forms.Button();
+            this.btnYAxis = new System.Windows.Forms.Button();
+            this.btnXAxis = new System.Windows.Forms.Button();
+            this.btnRefEdge = new System.Windows.Forms.Button();
+            this.btnLength = new System.Windows.Forms.Button();
+            this.btnPoint = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDeleteObj = new System.Windows.Forms.Button();
+            this.btnClearObj = new System.Windows.Forms.Button();
+            this.btnAddModel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbViewMode = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1264, 681);
+            this.splitContainer1.SplitterDistance = 300;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnCross);
+            this.groupBox3.Controls.Add(this.btnBracket);
+            this.groupBox3.Controls.Add(this.btnCurve);
+            this.groupBox3.Controls.Add(this.btnYAxis);
+            this.groupBox3.Controls.Add(this.btnXAxis);
+            this.groupBox3.Controls.Add(this.btnRefEdge);
+            this.groupBox3.Controls.Add(this.btnLength);
+            this.groupBox3.Controls.Add(this.btnPoint);
+            this.groupBox3.Location = new System.Drawing.Point(12, 138);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(277, 305);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Measure";
+            // 
+            // btnCross
+            // 
+            this.btnCross.Location = new System.Drawing.Point(6, 265);
+            this.btnCross.Name = "btnCross";
+            this.btnCross.Size = new System.Drawing.Size(265, 23);
+            this.btnCross.TabIndex = 10;
+            this.btnCross.Text = "Length Cross Point";
+            this.btnCross.UseVisualStyleBackColor = true;
+            this.btnCross.Click += new System.EventHandler(this.btnCross_Click);
+            // 
+            // btnBracket
+            // 
+            this.btnBracket.Location = new System.Drawing.Point(6, 230);
+            this.btnBracket.Name = "btnBracket";
+            this.btnBracket.Size = new System.Drawing.Size(265, 23);
+            this.btnBracket.TabIndex = 9;
+            this.btnBracket.Text = "Length With Bracket";
+            this.btnBracket.UseVisualStyleBackColor = true;
+            this.btnBracket.Click += new System.EventHandler(this.btnBracket_Click);
+            // 
+            // btnCurve
+            // 
+            this.btnCurve.Location = new System.Drawing.Point(6, 195);
+            this.btnCurve.Name = "btnCurve";
+            this.btnCurve.Size = new System.Drawing.Size(265, 23);
+            this.btnCurve.TabIndex = 8;
+            this.btnCurve.Text = "Length By Curve";
+            this.btnCurve.UseVisualStyleBackColor = true;
+            this.btnCurve.Click += new System.EventHandler(this.btnCurve_Click);
+            // 
+            // btnYAxis
+            // 
+            this.btnYAxis.Location = new System.Drawing.Point(6, 160);
+            this.btnYAxis.Name = "btnYAxis";
+            this.btnYAxis.Size = new System.Drawing.Size(265, 23);
+            this.btnYAxis.TabIndex = 7;
+            this.btnYAxis.Text = "Length By YAxis";
+            this.btnYAxis.UseVisualStyleBackColor = true;
+            this.btnYAxis.Click += new System.EventHandler(this.btnYAxis_Click);
+            // 
+            // btnXAxis
+            // 
+            this.btnXAxis.Location = new System.Drawing.Point(6, 125);
+            this.btnXAxis.Name = "btnXAxis";
+            this.btnXAxis.Size = new System.Drawing.Size(265, 23);
+            this.btnXAxis.TabIndex = 6;
+            this.btnXAxis.Text = "Length By XAxis";
+            this.btnXAxis.UseVisualStyleBackColor = true;
+            this.btnXAxis.Click += new System.EventHandler(this.btnXAxis_Click);
+            // 
+            // btnRefEdge
+            // 
+            this.btnRefEdge.Location = new System.Drawing.Point(6, 90);
+            this.btnRefEdge.Name = "btnRefEdge";
+            this.btnRefEdge.Size = new System.Drawing.Size(265, 23);
+            this.btnRefEdge.TabIndex = 5;
+            this.btnRefEdge.Text = "Length By Ref Edge";
+            this.btnRefEdge.UseVisualStyleBackColor = true;
+            this.btnRefEdge.Click += new System.EventHandler(this.btnRefEdge_Click);
+            // 
+            // btnLength
+            // 
+            this.btnLength.Location = new System.Drawing.Point(6, 55);
+            this.btnLength.Name = "btnLength";
+            this.btnLength.Size = new System.Drawing.Size(265, 23);
+            this.btnLength.TabIndex = 4;
+            this.btnLength.Text = "Length";
+            this.btnLength.UseVisualStyleBackColor = true;
+            this.btnLength.Click += new System.EventHandler(this.btnLength_Click);
+            // 
+            // btnPoint
+            // 
+            this.btnPoint.Location = new System.Drawing.Point(6, 20);
+            this.btnPoint.Name = "btnPoint";
+            this.btnPoint.Size = new System.Drawing.Size(265, 23);
+            this.btnPoint.TabIndex = 3;
+            this.btnPoint.Text = "Point";
+            this.btnPoint.UseVisualStyleBackColor = true;
+            this.btnPoint.Click += new System.EventHandler(this.btnPoint_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDeleteObj);
+            this.groupBox2.Controls.Add(this.btnClearObj);
+            this.groupBox2.Controls.Add(this.btnAddModel);
+            this.groupBox2.Location = new System.Drawing.Point(12, 75);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(277, 57);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Object";
+            // 
+            // btnDeleteObj
+            // 
+            this.btnDeleteObj.Location = new System.Drawing.Point(101, 20);
+            this.btnDeleteObj.Name = "btnDeleteObj";
+            this.btnDeleteObj.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteObj.TabIndex = 2;
+            this.btnDeleteObj.Text = "Delete";
+            this.btnDeleteObj.UseVisualStyleBackColor = true;
+            this.btnDeleteObj.Click += new System.EventHandler(this.btnDeleteObj_Click);
+            // 
+            // btnClearObj
+            // 
+            this.btnClearObj.Location = new System.Drawing.Point(196, 20);
+            this.btnClearObj.Name = "btnClearObj";
+            this.btnClearObj.Size = new System.Drawing.Size(75, 23);
+            this.btnClearObj.TabIndex = 1;
+            this.btnClearObj.Text = "Clear";
+            this.btnClearObj.UseVisualStyleBackColor = true;
+            this.btnClearObj.Click += new System.EventHandler(this.btnClearObj_Click);
+            // 
+            // btnAddModel
+            // 
+            this.btnAddModel.Location = new System.Drawing.Point(6, 20);
+            this.btnAddModel.Name = "btnAddModel";
+            this.btnAddModel.Size = new System.Drawing.Size(75, 23);
+            this.btnAddModel.TabIndex = 0;
+            this.btnAddModel.Text = "Model";
+            this.btnAddModel.UseVisualStyleBackColor = true;
+            this.btnAddModel.Click += new System.EventHandler(this.btnAddModel_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbViewMode);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(277, 57);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Model";
+            // 
+            // cbViewMode
+            // 
+            this.cbViewMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbViewMode.FormattingEnabled = true;
+            this.cbViewMode.Items.AddRange(new object[] {
+            "3D",
+            "3D / 2D",
+            "2D"});
+            this.cbViewMode.Location = new System.Drawing.Point(181, 22);
+            this.cbViewMode.Name = "cbViewMode";
+            this.cbViewMode.Size = new System.Drawing.Size(90, 20);
+            this.cbViewMode.TabIndex = 3;
+            this.cbViewMode.SelectedIndexChanged += new System.EventHandler(this.cbViewMode_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "View Mode";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(6, 20);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "VIZCoreXD.NET.Measure2D";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbViewMode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnDeleteObj;
+        private System.Windows.Forms.Button btnClearObj;
+        private System.Windows.Forms.Button btnAddModel;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnLength;
+        private System.Windows.Forms.Button btnPoint;
+        private System.Windows.Forms.Button btnCross;
+        private System.Windows.Forms.Button btnBracket;
+        private System.Windows.Forms.Button btnCurve;
+        private System.Windows.Forms.Button btnYAxis;
+        private System.Windows.Forms.Button btnXAxis;
+        private System.Windows.Forms.Button btnRefEdge;
+    }
+}
+
